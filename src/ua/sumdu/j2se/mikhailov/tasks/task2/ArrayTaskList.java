92c74dcad6b2549327998dd0ed8f255c8033576e
Mr-R119 <54175933+Mr-R119@users.noreply.github.com>
2021-09-06 14:03:56 +0400

    changed ArrayTaskList class

--- src/ua/sumdu/j2se/mikhailov/tasks/task2/ArrayTaskList.java
+++ src/ua/sumdu/j2se/mikhailov/tasks/task2/ArrayTaskList.java
@@ -3,72 +3,72 @@ package ua.sumdu.j2se.mikhailov.tasks.task2;
 public class ArrayTaskList  {
 
     private Task[] tasks;
-    private int index = 0;
-
+    private int size = 0;
 
     public void add(Task task) {
+
         if (tasks == null) {
             tasks = new Task[10];
         }
 
-        if (index < tasks.length) {
-            tasks[index] = task;
+        if (size < tasks.length) {
+            tasks[size] = task;
         } else {
-            Task[] tasks1 = new Task[index + 10];
-            System.arraycopy(tasks, 0, tasks1, 0, index);
-            tasks1[index] = task;
+            Task[] tasks1 = new Task[size + 10];
+            System.arraycopy(tasks, 0, tasks1, 0, size);
+            tasks1[size] = task;
             tasks = tasks1;
         }
-        index++;
+        size++;
     }
-
 
     public boolean remove(Task task) {
-        boolean result = false;
 
-        for (int i = 0; i < tasks.length; ++i) {
-            if (tasks[i] == task) {
-                tasks[i] = null;
-                index--;
-                result = true;
-                break;
-            }
-        }
-
-        if (result) {
-            Task[] tasks1 = new Task[index];
-            int pos = 0;
-            for (Task task1 : tasks) {
-                if (task1 != null) {
-                    tasks1[pos] = task1;
-                    pos++;
+        if (task == null) {
+            for (int index = 0; index < size; index++)
+                if (tasks[index] == null) {
+                    fastRemove(index);
+                    return true;
                 }
-            }
-            tasks = tasks1;
+        } else {
+            for (int index = 0; index < size; index++)
+                if (task == tasks[index]) {
+                    fastRemove(index);
+                    return true;
+                }
         }
-        return result;
+        return false;
+
     }
 
+    private void fastRemove(int index) {
+        int numMoved = size - index - 1;
+        if (numMoved > 0)
+            System.arraycopy(tasks, index + 1, tasks, index,
+                    numMoved);
+        tasks[--size] = null; // clear to let GC do its work
+    }
 
     public int size() {
-        int count = 0;
-        for (Task task : tasks)
-            if (task != null) ++count;
-        return count;
+        return size;
     }
 
-
     public Task getTask(int index) {
-        if (index < 0) {
-            throw new IllegalArgumentException("Index of task must be greater than 0");
-        }
+        rangeCheck(index);
+
         return tasks[index];
+    }
+
+    private void rangeCheck(int index){
+        if(index >= size){
+            throw new IndexOutOfBoundsException("Index: " + index + "Size: " + size);
+        }
     }
 
 
     public ArrayTaskList incoming(int from, int to){
         ArrayTaskList incomingTasks = new ArrayTaskList();
-        for(int i = 0; i < index; i++){
+        for(int i = 0; i < size; i++){
             if(tasks[i].nextTimeAfter(from) != -1 && tasks[i].nextTimeAfter(from) <= to)
                 incomingTasks.add(tasks[i]);
         }
